4ee85a9c68646adb7f62112dbfc4ca2c880fef69
Ravi5151 <vasamsettiravi5@gmail.com>
2024-06-28 18:53:18 +0530

    headings created

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,4 @@
+import { Component } from "react";
 import Header from "../Header/index";
 
 import {
@@ -5,22 +6,77 @@ import {
   HeaderContainer,
   TopContianer,
   BackgroundImage,
+  HeadingContainer,
+  Heading,
+  AnyWhereText,
+  FormHeading,
+  Label,
+  EmailContainer,
+  Form,
+  FormContiaer,
+  Input,
 } from "./styledComponent";
 
-const Home = () => {
-  return (
-    <MainContainer>
-      <TopContianer>
-        <BackgroundImage
-          src="https://res.cloudinary.com/dxa4rbmrj/image/upload/v1719487647/IN-en-20240624-popsignuptwoweeks-perspective_alpha_website_small_viysb6.jpg"
-          alt="cinima"
-        />
-        <HeaderContainer>
-          <Header />
-        </HeaderContainer>
-      </TopContianer>
-    </MainContainer>
-  );
-};
+class Home extends Component {
+  state = { email: "", focussed: false };
+
+  updateFocussedToTrue = () => {
+    this.setState({ focussed: true });
+  };
+
+  updateFocussedToFalse = () => {
+    this.setState({ focussed: false });
+  };
+
+  updateEmail = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  render() {
+    const { focussed, value } = this.state;
+
+    return (
+      <MainContainer>
+        <TopContianer>
+          <BackgroundImage
+            src="https://res.cloudinary.com/dxa4rbmrj/image/upload/v1719487647/IN-en-20240624-popsignuptwoweeks-perspective_alpha_website_small_viysb6.jpg"
+            alt="cinima"
+          />
+          <HeaderContainer>
+            <Header />
+          </HeaderContainer>
+          <HeadingContainer>
+            <Heading>Unlimited Movies, TV shows and more</Heading>
+            <AnyWhereText>Watch anywhere. Cancel anytime</AnyWhereText>
+            <FormContiaer>
+              <Form>
+                <FormHeading>
+                  Ready to Watch? Enter your email to create or restart your
+                  membership.
+                </FormHeading>
+                <EmailContainer>
+                  <Label
+                    htmlfof="email"
+                    focussed={focussed}
+                    hasValue={value !== ""}
+                  >
+                    Email address
+                  </Label>
+                  <Input
+                    type="email"
+                    id="email"
+                    onChange={this.updateEmail}
+                    onFocus={this.updateFocussedToTrue}
+                    onBlur={this.updateFocussedToFalse}
+                  />
+                </EmailContainer>
+              </Form>
+            </FormContiaer>
+          </HeadingContainer>
+        </TopContianer>
+      </MainContainer>
+    );
+  }
+}
 
 export default Home;
--- src/components/Home/styledComponent.js
+++ src/components/Home/styledComponent.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
-  width: 100vw;
+  width: 98vw;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
@@ -24,7 +24,7 @@ export const HeaderContainer = styled.div`
   background: linear-gradient(
     to bottom,
     rgba(0, 0, 0, 1.8) 0%,
-    rgba(0, 0, 0, 0.1) 50%,
+    rgba(0, 0, 0, 0.3) 50%,
     rgba(0, 0, 0, 1.5) 100%
   );
   opacity: 1;
@@ -36,10 +36,93 @@ export const TopContianer = styled.div`
   position: relative;
   display: inline-block;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const BackgroundImage = styled.img`
   width: 100%;
   display: block;
   height: auto;
+`;
+
+export const HeadingContainer = styled.div`
+  position: absolute;
+  display: inline-block;
+  width: 88%;
+  height: 100%;
+  color: white;
+  margin: auto;
+  margin-top: 20rem;
+  text-align: center;
+`;
+
+export const Heading = styled.h1`
+  font-size: 50px;
+  margin-bottom: 0px;
+`;
+
+export const AnyWhereText = styled.p`
+  font-size: 25px;
+  margin-top: 0px;
+`;
+
+export const FormHeading = styled.h1`
+  font-size: 22px;
+  font-weight: 400;
+`;
+
+export const Label = styled.label`
+  top: ${(props) => (props.focussed || props.hasValue ? "-20px" : "20px")};
+  left: 0;
+  transform: translateY(
+    ${(props) => (props.focussed || props.hasValue ? "0" : "-50%")}
+  );
+  font-size: ${(props) => (props.focussed || props.hasValue ? "12px" : "24px")};
+  color: ${(props) => (props.focussed || props.hasValue ? "#999" : "#fff")};
+  transition: all 0.3s ease;
+  pointer-events: none;
+  font-size: 20px;
+  color: #ffffff;
+  position: absolute;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  font-size: 18px;
+  border: none;
+  border-radius: 4px;
+  outline: none;
+  box-sizing: border-box;
+  background-color: transparent;
+  color: #fff;
+`;
+
+export const EmailContainer = styled.div`
+  border: 1px solid #f2f4f4;
+  padding: 5px;
+  position: relative;
+  width: auto;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  border-radius: 5px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const FormContiaer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
